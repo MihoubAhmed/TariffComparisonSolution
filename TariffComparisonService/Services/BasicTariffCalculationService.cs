d836cfdfc9021582a62ae139dcb86188479001aa
Mihoub Ahmed <MihoubAhmed@live.fr>
2025-04-08 18:22:01 +0200

    #13 Create BasicTariffCalculationService implementing Basic tariff calculation logic

---
+++ TariffComparisonService/Services/BasicTariffCalculationService.cs
@@ -0,0 +1,20 @@
+﻿using TariffComparisonService.Models;
+
+namespace TariffComparisonService.Services
+{
+    public class BasicTariffCalculationService : ITariffCalculationStrategy
+    {
+        public int SupportedTariffTypeId => 1; // 1 = Basic
+
+        public decimal CalculateAnnualCost(TariffProduct product, int consumption)
+        {
+            if (product == null)
+                throw new ArgumentNullException(nameof(product));
+
+            decimal baseCostPerYear = product.BaseCost * 12;
+            decimal consumptionCost = consumption * (product.AdditionalKwhCost.GetValueOrDefault() / 100m);
+            return baseCostPerYear + consumptionCost;
+        }
+    }
+
+}
